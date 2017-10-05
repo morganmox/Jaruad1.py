12d3ab0a08fa1cc29dbe3debd593103773ca6482
morganmox <30286286+morganmox@users.noreply.github.com>
2017-10-06 01:06:47 +0700

    รายละเอียดต่างๆ
    -ปรับแก้ตรรกะด้านการทะลุจอ
    -แสดงคะแนน
    -เพิ่มคะแนนเมื่อศัตรูตาย
    -แก้ไขสีพื้นหลัง

--- jaruad1.py
+++ jaruad1.py
@@ -42,16 +42,21 @@ class Ship(arcade.Sprite):
         if self.center_x<0:
             self.center_x = SCREEN_WIDTH
         if self.center_y>SCREEN_HEIGHT:
-            self.center_y = 0
+            self.center_y = SCREEN_HEIGHT-25
         if self.center_y<0:
-            self.center_y = SCREEN_HEIGHT  
+            self.center_y = 25  
 
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT)
- 
-        arcade.set_background_color(arcade.color.BLACK)
+
+        #whatever setting
+        arcade.set_background_color(arcade.color.AMAZON)
+        self.set_mouse_visible(False)
         self.framecount = 0
+        #scoring
+        self.score = 0
+        self.score_text = None
         #create all sprite array
         self.all_sprites_list = arcade.SpriteList()
         self.enemy_sprites_list = arcade.SpriteList()
@@ -64,25 +69,30 @@ class SpaceGameWindow(arcade.Window):
         #simply draw everything
         arcade.start_render()
         self.all_sprites_list.draw()
+        #scoreboard
+        output = f"Score : {self.score}"
+        if not self.score_text or output != self.score_text.text:
+            self.score_text = arcade.create_text(output, arcade.color.WHITE, 20)
+        arcade.render_text(self.score_text, SCREEN_WIDTH/2-50, SCREEN_HEIGHT-60)
 
     def update(self,x):
         self.all_sprites_list.update()
         self.framecount+=1
         #spawning enemy
-        if self.framecount>10:
+        if self.framecount>15:
             self.framecount = 0
-            for i in range(random.randrange(3)):
-                enemy = Enemy("images/enemy.png", SCALE)
-                self.enemy_sprites_list.append(enemy)
-                self.all_sprites_list.append(enemy)
+            enemy = Enemy("images/enemy.png", SCALE)
+            self.enemy_sprites_list.append(enemy)
+            self.all_sprites_list.append(enemy)
         #collision checking (bullet vs enemy)
         for bullet in self.bullet_sprites_list:
             hit = arcade.check_for_collision_with_list(bullet,self.enemy_sprites_list)
             #both bullet and enemy got killed for collision
-            if len(hit)>0:
+            if len(hit)!=0:
                 bullet.kill()
             for enemy in hit:
                 enemy.kill()
+                self.score+=1
             
     def on_key_press(self, symbol, modifiers):
         #pew pew
