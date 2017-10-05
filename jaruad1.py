aebae60677dbebf7db44aea265f9e27f87fa4e61
morganmox <30286286+morganmox@users.noreply.github.com>
2017-10-05 21:58:16 +0700

    แบ่ง class, อัพเดตหน้าตาตัวละคร

--- jaruad1.py
+++ jaruad1.py
@@ -1,56 +1,58 @@
 import arcade
- 
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
+SCALE = 1
+
+class Ship(arcade.Sprite):
+    def __init__(self,filename,scale):
+        super().__init__(filename,scale)
+        self.center_x = SCREEN_WIDTH/2
+        self.center_y = SCREEN_HEIGHT/2
+        self.vx = 0
+        self.vy = 0
+
+    def update(self):
+        self.center_x+=self.vx*5
+        self.center_y+=self.vy*5
+
+        super().update()
 
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
-        super().__init__(width, height)
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT)
  
         arcade.set_background_color(arcade.color.BLACK)
+        self.all_sprites_list = arcade.SpriteList()
+        self.player_sprite = Ship("images/ship.png", SCALE)
+        self.all_sprites_list.append(self.player_sprite)
 
-        self.v_x = 0
-        self.v_y = 0
-        self.ship = arcade.Sprite('images/ship.png')
-        self.ship.set_position(400,400)
- 
     def on_draw(self):
         arcade.start_render()
-        self.ship.draw()
+        self.all_sprites_list.draw()
+
+    def update(self,x):
+        self.all_sprites_list.update()
 
     def on_key_press(self, symbol, modifiers):
-        ship = self.ship
         if symbol == arcade.key.LEFT:
-            self.v_x=-1
-        if symbol == arcade.key.RIGHT:
-            self.v_x=1
+            self.player_sprite.vx = -1
+        elif symbol == arcade.key.RIGHT:
+            self.player_sprite.vx = 1
         if symbol == arcade.key.UP:
-            self.v_y=1
-        if symbol == arcade.key.DOWN:
-            self.v_y=-1
+            self.player_sprite.vy = 1
+        elif symbol == arcade.key.DOWN:
+            self.player_sprite.vy = -1
             
     def on_key_release(self, symbol, modifiers):
-        ship = self.ship
         if symbol == arcade.key.LEFT:
-            self.v_x=0
-        if symbol == arcade.key.RIGHT:
-            self.v_x=0
+            self.player_sprite.vx = 0
+        elif symbol == arcade.key.RIGHT:
+            self.player_sprite.vx = 0
         if symbol == arcade.key.UP:
-            self.v_y=0
-        if symbol == arcade.key.DOWN:
-            self.v_y=0
-        
-    def update(self, delta):
-        ship = self.ship
-        ship.set_position(ship.center_x+(self.v_x*5), ship.center_y+(self.v_y*5))        
-        if ship.center_y > SCREEN_HEIGHT:
-            ship.center_y = 0
-        if ship.center_y < 0:
-            ship.center_y = SCREEN_HEIGHT
-        if ship.center_x > SCREEN_WIDTH:
-            ship.center_x = 0
-        if ship.center_x < 0:
-            ship.center_x = SCREEN_WIDTH           
+            self.player_sprite.vy = 0
+        elif symbol == arcade.key.DOWN:
+            self.player_sprite.vy = 0            
  
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
