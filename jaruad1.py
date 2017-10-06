365f68ce6276a078dc295f70eab8fad297030599
morganmox <30286286+morganmox@users.noreply.github.com>
2017-10-06 21:44:43 +0700

    เรือดำน้ำ (LV2)

--- jaruad1.py
+++ jaruad1.py
@@ -1,5 +1,5 @@
 import arcade
-from models import Enemy,Bullet,Ship
+from models import Enemy,Bullet,Ship,EnemySubmarine,Torpedo
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
@@ -12,16 +12,22 @@ class SpaceGameWindow(arcade.Window):
         #whatever setting
         arcade.set_background_color(arcade.color.AMAZON)
         self.set_mouse_visible(False)
-        self.framecount = 0
+        self.framecount = 0 #enemy
+        self.framecount2 = 0 #enemysubmarine
+        self.framecount3 = 0 #torpedo
         #scoring/hpremaining
         self.score = 0
         self.score_text = None
         self.hp = 3
         self.hp_text = None
         self.gameover = False
+        #Level
+        self.lv2 = False
         #create all sprite array
         self.all_sprites_list = arcade.SpriteList()
         self.enemy_sprites_list = arcade.SpriteList()
+        self.enemysub_sprites_list = arcade.SpriteList()
+        self.torpedo_sprites_list = arcade.SpriteList()
         self.bullet_sprites_list = arcade.SpriteList()
         #giving birth to player
         self.player_sprite = Ship("images/ship.png", SCALE)
@@ -45,26 +51,59 @@ class SpaceGameWindow(arcade.Window):
     def update(self,x):
         self.all_sprites_list.update()
         self.framecount+=1
+
         #spawning enemy
-        if self.framecount>10 and self.gameover!=True:
+        if self.framecount>8 and self.gameover!=True:
             self.framecount = 0
             enemy = Enemy("images/enemy.png", SCALE)
             self.enemy_sprites_list.append(enemy)
             self.all_sprites_list.append(enemy)
-        #collision checking (bullet vs enemy)
+
+        #spawning enemy level 2
+        if self.score>=50:
+            self.lv2 = True
+            self.framecount2+=1
+            self.framecount3+=1
+            if self.framecount3 > 25:
+                self.framecount3 = 0
+                for enemysub in self.enemysub_sprites_list:
+                    torpedo = Torpedo("images/torpedo.png", SCALE)
+                    torpedo.center_x = enemysub.center_x
+                    torpedo.top = enemysub.bottom
+                    self.torpedo_sprites_list.append(torpedo)
+                    self.all_sprites_list.append(torpedo)
+                
+        if self.framecount2>70 and self.gameover!=True and self.lv2 == True:
+            self.framecount2 = 0
+            enemysub = EnemySubmarine("images/enemysub.png", SCALE)
+            self.enemysub_sprites_list.append(enemysub)
+            self.all_sprites_list.append(enemysub)
+
+        #collision checking (bullet vs enemies)
         for bullet in self.bullet_sprites_list:
             hit = arcade.check_for_collision_with_list(bullet,self.enemy_sprites_list)
-            #both bullet and enemy got killed for collision
             if len(hit)!=0:
                 bullet.kill()
             for enemy in hit:
                 enemy.kill()
                 self.score+=1
-        #collision checking (enemy vs player)       
-        hit2 = arcade.check_for_collision_with_list(self.player_sprite,self.enemy_sprites_list)
-        if len(hit2)!=0:
-            for enemy in hit2:
+            hit2 = arcade.check_for_collision_with_list(bullet,self.enemysub_sprites_list)
+            if len(hit2)!=0:
+                bullet.kill()
+            for enemysub in hit2:
+                enemysub.kill()
+                self.score+=2
+
+        #collision checking (enemies vs player)       
+        hit = arcade.check_for_collision_with_list(self.player_sprite,self.enemy_sprites_list)
+        if len(hit)!=0:
+            for enemy in hit:
                 enemy.kill()
+            self.hp-=1
+        hit2 = arcade.check_for_collision_with_list(self.player_sprite,self.torpedo_sprites_list)
+        if len(hit2)!=0:
+            for torpedo in hit2:
+                torpedo.kill()
             self.hp-=1
         #gameover status
         if self.hp == 0:
--- models.py
+++ models.py
@@ -1,9 +1,29 @@
 import arcade
 import random
-
+#Must match with main program's setting for accurate display
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 SCALE = 1.5
+
+class EnemySubmarine(arcade.Sprite):
+    def __init__(self,filename,scale):
+        super().__init__(filename,scale)
+        self.center_y = SCREEN_HEIGHT-random.randrange(200)-10
+        self.center_x = SCREEN_WIDTH
+        
+    def update(self):
+        self.center_x-=5
+        if self.center_x<0:
+            self.kill()
+
+class Torpedo(arcade.Sprite):
+    def __init__(self,filename,scale):
+        super().__init__(filename,scale)
+
+    def update(self):
+        self.center_y-=10
+        if self.center_y<0:
+            self.kill()
 
 class Enemy(arcade.Sprite):
     def __init__(self,filename,scale):
@@ -12,7 +32,7 @@ class Enemy(arcade.Sprite):
         self.center_x = random.randrange(SCREEN_WIDTH)
         
     def update(self):
-        self.center_y-=5
+        self.center_y-=8
         if self.center_y<0:
             self.kill()
 
@@ -21,7 +41,7 @@ class Bullet(arcade.Sprite):
         super().__init__(filename,scale)
 
     def update(self):
-        self.center_y+=8
+        self.center_y+=10
         if self.center_y>SCREEN_HEIGHT:
             self.kill()
 
