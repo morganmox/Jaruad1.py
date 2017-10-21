f54b7138ca1c1cf84fb04ea8368ed03565acc858
morganmox <30286286+morganmox@users.noreply.github.com>
2017-10-21 18:03:05 +0700

    พิมพ์ 10 อันดับแรกหลังจบเกม

--- scoresort.py
+++ scoresort.py
@@ -6,9 +6,23 @@ with open("Score.txt") as scoree:
             score  = int(score)
             scores.append((score,name))
 scores.sort(key=lambda s:s[0])
+
 text_file = open("Score.txt","w")
 for score,name in reversed(scores):
     text_file.write(str(score))
     text_file.write(":")
     text_file.write(name)
 text_file.close()
+
+highscore = "\n***HIGH SCORE***\n"
+text_file = open("Score.txt","r")
+rank = 1
+while rank<=10 :
+    n = text_file.readline()
+    if len(n)>0:
+        highscore=highscore+"\n["+str(rank)+"] ."+n
+        rank+=1
+    else:
+        break
+text_file.close()
+print(highscore)
